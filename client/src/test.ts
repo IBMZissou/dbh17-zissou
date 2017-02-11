61a2721880e467df8f38dc7ecac92ba81820fa14
Lourens Elzinga <LFElzingaCIC@nl.ibm.com>
2017-02-11 12:09:27 +0100

    Changed declare const to declare let for karma globals.

--- client/src/test.ts
+++ client/src/test.ts
@@ -9,8 +9,8 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare const __karma__: any;
-declare const require: any;
+declare let __karma__: any;
+declare let require: any;
 
 // Prevent Karma from running prematurely.
 __karma__.loaded = function () {};
