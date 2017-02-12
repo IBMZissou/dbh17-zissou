f62650b61ad69e7bc999309f9505bc736c1ec013
Lourens Elzinga <LFElzingaCIC@nl.ibm.com>
2017-02-12 09:40:06 +0100

    Added company search

--- client/src/app/components/dashboard/new-project/client-info/client-info.component.ts
+++ client/src/app/components/dashboard/new-project/client-info/client-info.component.ts
@@ -1,9 +1,12 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { ProjectWizardData } from '../../../../models/project-wizard-data.model';
+import { CompanyInfoService } from '../../../../services/companyinfo.service';
+import { KvKCompany } from '../../../../models/kvkcompany.model';
 
 @Component({
   selector: 'np-client-info',
-  templateUrl: './client-info.component.html'
+  templateUrl: './client-info.component.html',
+  styleUrls: [ './client-info.component.scss' ]
 })
 export class NewProjectClientInfoComponent {
   @Input()
@@ -14,4 +17,45 @@ export class NewProjectClientInfoComponent {
 
   @Output()
   public nextClicked = new EventEmitter<void>();
+
+  public searchingCompany = false;
+
+  public constructor(
+    private companyInfoService: CompanyInfoService
+  ) {}
+
+  public searchCompany(identifier: string): void {
+    if (!identifier) {
+      return;
+    }
+
+    this.searchingCompany = true;
+
+    if (this.isNumeric(identifier)) {
+      this.companyInfoService.findByKvKNumber(identifier).finally(() => this.searchingCompany = false).subscribe(
+        (result: {companies: KvKCompany[]}) => this.populateFromSearch(result.companies)
+      );
+    } else {
+      this.companyInfoService.findByName(identifier).finally(() => this.searchingCompany = false).subscribe(
+        (result: {companies: KvKCompany[]}) => this.populateFromSearch(result.companies)
+      );
+    }
+  }
+
+  private isNumeric(value: string): boolean {
+    return /^\d+$/.test(value);
+  }
+
+  private populateFromSearch(result: KvKCompany[]) {
+    if (!result || result.length !== 1) {
+      return;
+    }
+
+    let company = result[0];
+
+    this.project.client.companyName = company.businessName;
+    this.project.client.kvkNumber = company.kvknummer;
+    this.project.client.address = (company.street + ' ' + company.houseNumber + ' ' + company.houseNumberAddition).trim();
+    this.project.client.zipcode = company.postalCode;
+  }
 }
