b8b0d4ed66f2979e795104f9bea0bd919b2ace7c
Luuc Oosterveld <luuc.oosterveldisc@nl.ibm.com>
2017-02-12 10:32:02 +0100

    add help button and warning messages for questionnaire whenever clicking no

--- client/src/app/components/dashboard/new-project/questionnaire/questionnaire.component.ts
+++ client/src/app/components/dashboard/new-project/questionnaire/questionnaire.component.ts
@@ -1,10 +1,21 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, trigger, state, style, transition, animate } from '@angular/core';
 import { ProjectWizardData } from '../../../../models/project-wizard-data.model';
 
 @Component({
   selector: 'np-questionnaire',
   templateUrl: './questionnaire.component.html',
-  styleUrls: [ './questionnaire.component.scss' ]
+  styleUrls: [ './questionnaire.component.scss' ],
+  animations: [
+    trigger('collapse',[
+      state('open', style({
+        height: '*'
+      })),
+      state('closed', style({
+        height: 0
+      })),
+      transition('open <=> closed', animate(300))
+    ])
+  ]
 })
 export class NewProjectQuestionnaireComponent {
   @Input()
@@ -16,6 +27,7 @@ export class NewProjectQuestionnaireComponent {
   @Output()
   public nextClicked = new EventEmitter<void>();
 
+  public collapse = true;
   public eligible = true;
   public currentQuestion = 0;
   public progress = 0;
@@ -33,6 +45,31 @@ export class NewProjectQuestionnaireComponent {
     'Will your payment be withheld in case of illness?',
     'When you are too busy or in case of illness, can you decide who will replace you?'
   ];
+  public information = [
+    'You are taking part in an economic transaction whenever you deliver goods or services to ' +
+    'private individuals or companies that in exchange will compensate you in a way that isn’t symbolical.',
+    'Entrepreneurship involves choosing which jobs you do as well as the way you execute them. Not being able ' +
+    'to make work related decisions could be an indication of not being an entrepreneur.',
+    'To become less dependent you should invest in your own equipment as well as use your own equipment ' +
+    'during jobs. If your job involves delivering goods it is perfectly possible you need to work with your ' +
+    'client’s equipment, in this case not using your own equipment is not a problem.',
+    'Working for only one client for a long time and retrieve almost all of your income from one client could ' +
+    'very well be an indication of not being an entrepreneur.',
+    'Entrepreneurs should be actively finding new clients. Some examples are: having your own website, ' +
+    'advertise, being a member of a network association etc.',
+    'Entrepreneurship involves deciding your own rates. It could be an indication of not being an entrepreneur ' +
+    'when your client decides your rates.',
+    'Third parties doing your invoicing could be an indication of not being an entrepreneur.',
+    'Entrepreneurial risk is part of being an entrepreneur. If there is no financial risk, this is an ' +
+    'indication of not being an entrepreneur.',
+    'Not being liable for your own debt is an indication of not being an entrepreneur.',
+    'Not performing properly should be at one’s own expense, if not, this is in indication of not being an ' +
+    'entrepreneur.',
+    'Other than working as an employee you will not be paid wages whenever you fall ill, this means you ' +
+    'are not an entrepreneur.',
+    'When delivering services, it’s perfectly possible you’re irreplaceable, but when this is not the case, ' +
+    'you should make agreements about replacement during illness and in case of over-occupation.'
+  ];
 
   public yesClicked(): void {
     ++this.currentQuestion;
@@ -45,5 +82,10 @@ export class NewProjectQuestionnaireComponent {
 
   public noClicked(): void {
     this.eligible = false;
+  }
+
+  public resume(): void {
+    this.eligible = true;
+    this.yesClicked();
   }
 }
